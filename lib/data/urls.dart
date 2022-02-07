ef3aa40bb3efbbf4ca947e00be07a1965562a9ce
Iman khoshabi <iman.neofight@gmail.com>
2022-02-08 01:31:14 +0330

    Implement minimal home

---
+++ lib/data/urls.dart
@@ -0,0 +1,5 @@
+class Urls {
+  static const String branch = "master";
+  static const String baseUrl = "https://raw.githubusercontent.com/imaNNeoFighT/steamy_draw/$branch";
+  static const String appDataUrl = "$baseUrl/statics/app_data.json";
+}
---
+++ lib/resources.dart
@@ -0,0 +1,11 @@
+class AppTexts {
+  static const String appName = "بخار روی عکس";
+}
+
+class AppColors {
+
+}
+
+class AppSizes {
+
+}
